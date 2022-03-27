0f969f57d573bd896cd2b59900f4dc04a47f661d
Dylan Pina <dillon_idgafos@yahoo.com>
2022-03-26 21:09:09 -0400

    Clicking on a coin in watchlist routes to coin page

--- src/components/Authentication/UserSidebar.js
+++ src/components/Authentication/UserSidebar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import { CryptoState } from "../../context/CryptoContext";
@@ -39,7 +40,16 @@ const useStyles = makeStyles({
 		height: "5%",
 		width: "100%",
 		backgroundColor: "rgba(139, 69, 255, 0.8)",
+		boxShadow: "0 0 10px rgb(13, 13, 13, 0.8)",
+		borderRadius: 100,
+		fontFamily: "Montserrat",
+		fontSize: 16,
+		fontWeight: 700,
 		marginTop: 20,
+		"&:hover": {
+			backgroundColor: "rgba(139, 69, 255, 0.8)",
+			transform: "translateY(-1px)",
+		},
 	},
 	watchlist: {
 		flex: 1,
@@ -56,6 +66,7 @@ const useStyles = makeStyles({
 		boxShadow: "0 0 10px rgb(13, 13, 13, 0.6)",
 	},
 	coin: {
+		cursor: "pointer",
 		padding: 10,
 		borderRadius: 5,
 		color: "black",
@@ -64,7 +75,16 @@ const useStyles = makeStyles({
 		justifyContent: "space-between",
 		alignItems: "center",
 		backgroundColor: "rgba(139, 69, 255, 1)",
-		boxShadow: "0 0 3px black",
+		boxShadow: "0 0 8px black",
+	},
+	deleteIcon: {
+		cursor: "pointer",
+		fontSize: 16,
+		"&:hover": {
+			fill: "red",
+			fontSize: 17,
+			transform: "translateY(-0.5px)",
+		},
 	},
 });
 
@@ -74,6 +94,8 @@ const UserSidebar = () => {
 	});
 
 	const { user, setAlert, watchlist, coins, symbol } = CryptoState();
+
+	const navigate = useNavigate();
 
 	console.log(coins);
 
@@ -124,7 +146,7 @@ const UserSidebar = () => {
 				<React.Fragment key={anchor}>
 					<Avatar
 						onClick={toggleDrawer(anchor, true)}
-						style={{ height: 38, width: 38, cursor: "pointer", backgroundColor: "rgba(139, 69, 255, 0.8)", boxShadow: "2px 2px 10px black"}}
+						style={{ height: 38, width: 38, cursor: "pointer", backgroundColor: "rgba(139, 69, 255, 0.8)", boxShadow: "2px 2px 10px black" }}
 						src={user.photoURL}
 						alt={user.displayName || user.email}
 					/>
@@ -132,19 +154,32 @@ const UserSidebar = () => {
 						<div className={classes.container}>
 							<div className={classes.profile}>
 								<Avatar className={classes.picture} src={user.photoURL} alt={user.displayName || user.email} />
-								<span style={{ width: "100%", fontSize: 25, textAlign: "center", fontFamily: "Montserrat", fontWeight: 500, textShadow: "1px 1px 4px black"}}>
+								<span
+									style={{
+										width: "100%",
+										fontSize: 25,
+										textAlign: "center",
+										fontFamily: "Montserrat",
+										fontWeight: 500,
+										textShadow: "1px 1px 4px black",
+									}}
+								>
 									{user.displayName || user.email}
 								</span>
 								<div className={classes.watchlist}>
-									<span style={{fontWeight: 600, marginBottom: 15, fontFamily: "Montserrat", textShadow: "1px 1px 4px black" }}>Watch list</span>
+									<span style={{ fontWeight: 600, marginBottom: 15, fontFamily: "Montserrat", textShadow: "1px 1px 4px black" }}>Watch list</span>
 									{coins.map((coin) => {
 										if (watchlist.includes(coin.id))
 											return (
-												<div className={classes.coin}>
-													<span style={{fontFamily: "Montserrat"}}>{coin.name} ({coin.symbol.toUpperCase()})</span>
+												<div className={classes.coin} 
+												onClick={() => navigate(`/coins/${coin.id}`)}>
+													<span style={{ fontFamily: "Montserrat" }}>
+														{coin.name} ({coin.symbol.toUpperCase()})
+													</span>
 													<span style={{ display: "flex", gap: 8 }}>
-														{symbol}{numberWithCommas(coin.current_price.toFixed(2))}
-														<AiFillDelete style={{ cursor: "pointer" }} fontSize="16" onClick={() => removeFromWatchlist(coin)} />
+														{symbol}
+														{numberWithCommas(coin.current_price.toFixed(2))}
+														<AiFillDelete className={classes.deleteIcon} onClick={() => removeFromWatchlist(coin)} />
 													</span>
 												</div>
 											);
